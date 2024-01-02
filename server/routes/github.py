30932cacf6f2d797ce0bbdac7de57b1be47d3a27
jingfelix <jingfelix@outlook.com>
2024-01-02 17:00:50 +0800

    feat: GitHub Register (basic)
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- server/routes/github.py
+++ server/routes/github.py
@@ -3,14 +3,10 @@ import os
 
 from app import app
 from flask import Blueprint, abort, redirect, request, session
+from model.schema import Team
 from utils.auth import authenticated
-
-from server.utils.github.github import (
-    get_installation_token,
-    get_jwt,
-    verify_github_signature,
-)
-from server.utils.user import register
+from utils.github.common import get_installation_token, get_jwt, verify_github_signature
+from utils.user import register
 
 bp = Blueprint("github", __name__, url_prefix="/api/github")
 
@@ -20,34 +16,13 @@ bp = Blueprint("github", __name__, url_prefix="/api/github")
 def github_install():
     """Install GitHub App.
 
-    If not `installation_id`, redirect to install page.
-    If `installation_id`, get installation token.
-
-    If `code`, register by code.
+    Redirect to GitHub App installation page.
     """
     installation_id = request.args.get("installation_id", None)
-
     if installation_id is None:
         return redirect(
             f"https://github.com/apps/{os.environ.get('GITHUB_APP_NAME')}/installations/new"
         )
-
-    logging.debug(f"installation_id: {installation_id}")
-
-    jwt = get_jwt(
-        os.environ.get("GITHUB_APP_PRIVATE_KEY_PATH"),
-        os.environ.get("GITHUB_APP_ID"),
-    )
-
-    installation_token = get_installation_token(jwt, installation_id)
-    if installation_token is None:
-        logging.debug("Failed to get installation token.")
-
-        # TODO: 统一解决各类 http 请求失败的情况
-        abort(500)
-    logging.debug(f"installation_token: {installation_token}")
-
-    return "Success!"
 
 
 @bp.route("/register", methods=["GET"])
@@ -58,18 +33,20 @@ def github_register():
     If `code`, register by code.
     """
     code = request.args.get("code", None)
+
     if code is None:
         return redirect(
             f"https://github.com/login/oauth/authorize?client_id={os.environ.get('GITHUB_CLIENT_ID')}"
         )
 
+    # 通过 code 注册；如果 user 已经存在，则一样会返回 user_id
     user_id = register(code)
     if user_id is None:
         return "Failed to register by code."
 
+    # 保存用户注册状态
     session["user_id"] = user_id
 
-    # TODO: 统一的返回格式
     return "Success!"
 
 
--- server/utils/user.py
+++ server/utils/user.py
@@ -1,32 +1,44 @@
 from app import app, db
-from model.schema import BindUser, User
-
-from server.utils.github.github import oauth_by_code
+from model.schema import BindUser, ObjID, User
+from utils.github.common import get_user_info, oauth_by_code
 
 
 def register(code: str) -> str | None:
     """GitHub OAuth register.
 
-    If not `code`, redirect to GitHub OAuth page.
     If `code`, register by code.
     """
 
-    oauth_info = oauth_by_code(code)
+    oauth_info = oauth_by_code(code)  # 获取 access token
 
-    user_info = oauth_info.get("user", None)
+    access_token = oauth_info.get("access_token", None)[0]  # 这里要考虑取哪个，为什么会有多个？
 
     # 使用 oauth_info 中的 access_token 获取用户信息
+    user_info = get_user_info(access_token)
+
+    # 查询 github_id 是否已经存在，若存在，则返回 user_id
+    github_id = user_info.get("id", None)
+    if github_id is not None:
+        user = User.query.filter_by(github_id=github_id).first()
+        if user is not None:
+            return user.id
 
     new_user = User(
-        email=user_info.get("email", None),
-        name=user_info.get("login", None),  # TODO: 确认一下 login 和 name 哪个是唯一的
+        id=ObjID.new_id(),
+        github_id=github_id,
+        email=user_info.get(
+            "email", None
+        ),  # 这里的邮箱其实是公开邮箱，可能会获取不到 TODO: 换成使用用户邮箱 API 来获取
+        name=user_info.get("login", None),
         avatar=user_info.get("avatar_url", None),
         extra=user_info,
     )
 
     db.session.add(new_user)
+    db.session.commit()
 
     new_bind_user = BindUser(
+        id=ObjID.new_id(),
         user_id=new_user.id,
         platform="github",
         email=user_info.get("email", None),
