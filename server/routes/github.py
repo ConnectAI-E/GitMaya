29efecdf72b04356504016c43705b522a82d41ee
jingfelix <jingfelix@outlook.com>
2024-01-03 16:03:22 +0800

    feat: send msg via frontend
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- server/routes/github.py
+++ server/routes/github.py
@@ -1,7 +1,8 @@
+import json
 import os
 
 from app import app
-from flask import Blueprint, abort, redirect, request, session
+from flask import Blueprint, abort, jsonify, make_response, redirect, request, session
 from model.team import create_code_application, create_team
 from utils.auth import authenticated
 from utils.github.application import (
@@ -28,9 +29,6 @@ def github_install():
             f"https://github.com/apps/{os.environ.get('GITHUB_APP_NAME')}/installations/new"
         )
 
-    # 创建 Team
-    print(f"installation_id {installation_id}")
-
     jwt = get_jwt(
         os.environ.get("GITHUB_APP_PRIVATE_KEY_PATH"),
         os.environ.get("GITHUB_APP_ID"),
@@ -38,24 +36,41 @@ def github_install():
     installation_token = get_installation_token(jwt, installation_id)
     if installation_token is None:
         app.logger.error("Failed to get installation token.")
-        return "Failed to get installation token."
+        return jsonify({"message": "Failed to get installation token."}), 500
 
     app_info = get_installation_info(jwt, installation_id)
     if app_info is None:
         app.logger.error("Failed to get installation info.")
-        return "Failed to get installation info."
+        return jsonify({"message": "Failed to get installation info."}), 500
 
     # 判断安装者的身份是用户还是组织
-    type = app_info["account"]["type"]
-    if type == "user":
+    type: str = app_info["account"]["type"]
+    if type.lower() == "user":
         app.logger.error("User is not allowed to install.")
         # TODO: 定义与前端的交互数据格式
-        return "User is not allowed to install."
+        return jsonify({"message": "User is not allowed to install."}), 403
 
     team = create_team(app_info)
     code_application = create_code_application(team.id, installation_id)
 
-    return app_info
+    # TODO: 加入后台任务
+
+    return make_response(
+        """
+<script>
+try {
+  window.opener.postMessage("""
+        + json.dumps(dict(event="installation", data=app_info))
+        + """, '*')
+  setTimeout(() => window.close(), 3000)
+} catch(e) {
+  console.error(e)
+  location.replace('/')
+}
+</script>
+                                     """,
+        {"Content-Type": "text/html"},
+    )
 
 
 @bp.route("/oauth", methods=["GET"])
@@ -75,12 +90,27 @@ def github_register():
     # 通过 code 注册；如果 user 已经存在，则一样会返回 user_id
     user_id = register(code)
     if user_id is None:
-        return "Failed to register by code."
+        return jsonify({"message": "Failed to register."}), 500
 
     # 保存用户注册状态
     session["user_id"] = user_id
 
-    return "Success!"
+    return make_response(
+        """
+<script>
+try {
+  window.opener.postMessage("""
+        + json.dumps(dict(event="oauth", data={"user_id": user_id}))
+        + """, '*')
+  setTimeout(() => window.close(), 3000)
+} catch(e) {
+  console.error(e)
+  location.replace('/')
+}
+</script>
+                                     """,
+        {"Content-Type": "text/html"},
+    )
 
 
 @bp.route("/hook", methods=["POST"])
