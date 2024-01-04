d473f070d5e9154a0b41918a17eb1b71d95dda66
lloydzhou <lloydzhou@qq.com>
2024-01-04 13:56:50 +0800

    update

--- server/utils/lark/base.py
+++ server/utils/lark/base.py
@@ -2,10 +2,15 @@ from connectai.lark.sdk import *
 
 
 class GitMayaCardNote(FeishuMessageNote):
+    @property
+    def img_key(self):
+        # TODO 这里似乎应该按机器人生成不同的key，不同租户不同机器人，可能访问的权限是不一样的
+        return "img_v3_026k_3b6ce6be-4ede-46b0-96d7-61f051ff44fg"
+
     def __init__(self, content="GitMaya"):
         super().__init__(
             FeishuMessageImage(
-                img_key="img_v3_026k_3b6ce6be-4ede-46b0-96d7-61f051ff44fg",  # TODO 这里可能有权限问题
+                img_key=self.img_key,
                 alt="",
             ),
             FeishuMessagePlainText(content),
