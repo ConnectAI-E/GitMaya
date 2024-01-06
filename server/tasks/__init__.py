4f2cc223944755e6c249380872409d92cecf7f4d
jingfelix <jingfelix@outlook.com>
2024-01-06 15:17:34 +0800

    feat: add schedule task to pull GitHub Repos
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- server/tasks/__init__.py
+++ server/tasks/__init__.py
@@ -12,6 +12,19 @@ celery.conf.beat_schedule = {
         "schedule": timedelta(minutes=10),  # 定时1minutes执行一次
         "args": (),  # 函数传参的值
     },
+    # 定时创建群聊
+    # 不自动创建，手动创建
+    # "create_chat_group_for_all_repo": {
+    #     "task": "tasks.lark.create_chat_group_for_all_repo",
+    #     "schedule": timedelta(minutes=10),  # 定时1minutes执行一次
+    #     "args": (),  # 函数传参的值
+    # },
+    # 定时拉取（更新）所有 GitHub 侧信息
+    "pull_github_repo_all": {
+        "task": "tasks.github.pull_github_repo_all",
+        "schedule": timedelta(hours=24),
+        "args": (),
+    },
 }
 
 
