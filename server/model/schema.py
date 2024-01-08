0a4565e92d8cfc5cc2c7ae22a8b55e354d00ec48
lloydzhou <lloydzhou@qq.com>
2024-01-08 21:48:17 +0800

    feat: add new table

--- server/model/schema.py
+++ server/model/schema.py
@@ -318,7 +318,7 @@ class Issue(Base):
         nullable=True,
         comment="哪一个repo_id",
     )
-    name = db.Column(db.String(128), nullable=True, comment="名称")
+    title = db.Column(db.String(128), nullable=True, comment="名称")
     description = db.Column(db.String(1024), nullable=True, comment="描述")
     message_id = db.Column(db.String(128), nullable=True, comment="message_id")
     extra = db.Column(
@@ -334,7 +334,7 @@ class PullRequest(Base):
         nullable=True,
         comment="哪一个repo_id",
     )
-    name = db.Column(db.String(128), nullable=True, comment="名称")
+    title = db.Column(db.String(128), nullable=True, comment="名称")
     description = db.Column(db.String(1024), nullable=True, comment="描述")
     message_id = db.Column(db.String(128), nullable=True, comment="message_id")
     extra = db.Column(
