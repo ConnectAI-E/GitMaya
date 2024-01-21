1cf450187714893293e3c032bf9bc4d9893ed55a
lloydzhou <lloydzhou@qq.com>
2024-01-21 13:28:54 +0800

    hotfix: featch github repos and members

--- server/utils/github/organization.py
+++ server/utils/github/organization.py
@@ -16,10 +16,17 @@ class GitHubAppOrg(BaseGitHubApp):
         https://docs.github.com/zh/rest/repos/repos?apiVersion=2022-11-28#list-organization-repositories
         """
 
-        return self.base_github_rest_api(
-            f"https://api.github.com/orgs/{org_name}/repos",
-            auth_type="install_token",
-        )
+        page = 1
+        while True:
+            repos = self.base_github_rest_api(
+                f"https://api.github.com/orgs/{org_name}/repos?page={page}",
+                auth_type="install_token",
+            )
+            if len(repos) == 0:
+                break
+            for repo in repos:
+                yield repo
+            page = page + 1
 
     def get_org_members(self, org_name: str) -> list | None:
         """Get a list of members of an organization.
@@ -31,8 +38,14 @@ class GitHubAppOrg(BaseGitHubApp):
             list | None: A list of members of the organization.
         https://docs.github.com/zh/rest/orgs/members?apiVersion=2022-11-28#list-organization-members
         """
-
-        return self.base_github_rest_api(
-            f"https://api.github.com/orgs/{org_name}/members",
-            auth_type="install_token",
-        )
+        page = 1
+        while True:
+            members = self.base_github_rest_api(
+                f"https://api.github.com/orgs/{org_name}/members?page={page}",
+                auth_type="install_token",
+            )
+            if len(members) == 0:
+                break
+            for member in members:
+                yield member
+            page = page + 1
