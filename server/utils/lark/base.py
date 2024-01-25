1f40e7b413e5274903d1dbb097a9a5f6641b6cb0
Dylan Guo <80776877@qq.com>
2024-01-25 21:09:47 +0800

    fix

--- server/utils/lark/base.py
+++ server/utils/lark/base.py
@@ -36,17 +36,3 @@ class GitMayaCardNote(FeishuMessageNote):
             ),
             FeishuMessagePlainText(content),
         )
-
-
-def desc_to_feishu_message(desc):
-    messages = []
-    messages.append(
-        FeishuMessageDiv("💬  <font color='black'>**主要内容**</font>", tag="lark_md")
-    )
-
-    for part in desc:
-        if part.startswith("img_"):
-            messages.append(FeishuMessageImage(img_key=part, tag="img"))
-        else:
-            messages.append(FeishuMessageMarkdown(part))
-    return messages
